1f8df3c9f449043628bc454fa68676ef0764fe27
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-28 19:32:54 +0300

    add JSDocs to API functions

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //simple dev environment check so after deployment we don't have manually change the url everytime
-const isDev = window.location.hostname === 'location';
+const isDev = window.location.hostname === 'localhost';
 //if server is running on localhost, baseurl is set to be different from when it will be running on heroku
 const baseUrl = isDev ? 'http://localhost:5000/' : '/';
 
@@ -17,22 +17,33 @@ const STORY_URL = baseUrl + 'userstory';
 
 
 //get all userstories
+/**
+ * @returns {Promise} List of userstories
+ */
 export const getStories = () => axios.get(STORY_URL)
     .then(response => response.data)
     .catch(error => console.log(error));
 
 //get one userstory
+/**
+ * @param {string} storyId userstory id from database
+ * @returns {Promise} single userstory
+ */
 export const getStory = storyId => axios.get(STORY_URL + `/${storyId}`)
     .then(response => response.data)
     .catch(error => console.log(error));
 
 //create userstory
-//parameter is an object with property of title -> more added later?
+//takes an object as a parameter
+/**
+ * @param {Object} userstory created by user
+ * @param {string} userstory.title title of the userstory (required)
+ * @param {string} userstory.descr description of the userstory (optional)
+ * @returns {Promise} saved userstory
+ */
 export const createStory = async (story) => {
-    const savedStory = {title: story.title};
-
     try {
-        const response = await axios.post(STORY_URL, savedStory);
+        const response = await axios.post(STORY_URL, story);
         console.log('Story saved', response.data);
     } catch (error) {
         console.log('Story not saved', error);
@@ -42,6 +53,10 @@ export const createStory = async (story) => {
 //update userstory
 
 //delete
+/**
+ * @param {string} storyId userstory id from database
+ * @returns {Promise} deleted userstory
+ */
 export const deleteStory = async (storyId) => {
     const DELETE_URL = STORY_URL + `/${storyId}`;
     try {
