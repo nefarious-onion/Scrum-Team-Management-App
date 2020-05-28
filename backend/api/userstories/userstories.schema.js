697495fb813e628895f22d17e6878c8ec1967175
bea94k <beatakusnierz94@gmail.com>
2020-05-28 10:50:09 +0300

    Add title: required in story Schema

--- backend/api/userstories/userstories.schema.js
+++ backend/api/userstories/userstories.schema.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userstorySchema = new Schema({
-    title: String,
-    descr: String
+  title: {
+    type: String,
+    required: true,
+  },
+  descr: String,
 });
 
 // userstory model - creating in DB a collection called userstories
