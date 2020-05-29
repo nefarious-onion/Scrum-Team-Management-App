9251a809d1106f34a21fbda90952769a45598e8b
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-29 11:52:13 +0300

    testing api calls

--- frontend/src/components/BacklogView/BacklogView.js
+++ frontend/src/components/BacklogView/BacklogView.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BacklogList from './BacklogList/BacklogList'
 import SpritList from './SprintList/SprintList'
+import {deleteStory, updateStory, getStories, getStory } from '../../services/api.service';
+
 const BacklogView = () => {
+
+    useEffect(() => {
+        getStories().then(stories => console.log(stories));
+        //getStory('5ecfde3b840b345154b88569').then(story => console.log(story));
+        //deleteStory('5ecfdd89840b345154b8855d').then(story => console.log(story));
+    }, []);
+
+
     return (
         <div>
             <BacklogList />
--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -34,7 +34,7 @@ export const getStory = storyId => axios.get(STORY_URL + `/${storyId}`)
 //create userstory
 //takes an object as a parameter
 /**
- * @param {Object} userstory created by user
+ * @param {Object} userstory userstory as an object
  * @param {string} userstory.title title of the userstory (required)
  * @param {string} userstory.descr description of the userstory (optional)
  * @returns {Promise} saved userstory
@@ -45,5 +45,36 @@ export const createStory = async (story) => {
         console.log('Story saved', response.data);
     } catch (error) {
         console.log('Story not saved', error);
+    }
+}
+
+//update userstory
+/**
+ * @param {string} storyId userstory id from database
+ * @param {Object} updatedData updated userstory as an object
+ * @returns {Promise} updated userstory
+ */
+export const updateStory = async (storyId, updatedData) => {
+    const PATCH_URL = STORY_URL + `/${storyId}`;
+    try {
+        const response = await axios.patch(PATCH_URL, updatedData);
+        console.log('Story updated', response.data);
+    } catch (error) {
+        console.log('Story not uodated', error);
+    }
+}
+
+//delete
+/**
+ * @param {string} storyId userstory id from database
+ * @returns {Promise} deleted userstory
+ */
+export const deleteStory = async (storyId) => {
+    const DELETE_URL = STORY_URL + `/${storyId}`;
+    try {
+        const response = await axios.delete(DELETE_URL);
+        console.log('Story deleted', response.data);
+    } catch (error) {
+        console.log('Story not deleted', error);
     }
 }
