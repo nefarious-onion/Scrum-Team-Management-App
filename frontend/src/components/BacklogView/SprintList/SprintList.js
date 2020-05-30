625a51f3f83d33a9ebb353a47872ef84e7057bed
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-30 10:55:34 +0300

    add check for empty/undefined userstorylist

--- frontend/src/components/BacklogView/SprintList/SprintList.js
+++ frontend/src/components/BacklogView/SprintList/SprintList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Userstory from '../../Userstory/Userstory'
 const SprintList = ({ sprintloglist }) => {
+    //checks that the list is not empty or undefined
+    const isValid = sprintloglist !== undefined && sprintloglist.length > 0;
     //mapping sprintloglist from back-end
-    const storyList = sprintloglist.map(story =>
+    const storyList = isValid
+        ? sprintloglist.map(story =>
         <Userstory key={story._id} _id={story._id} title={story.title} desc={story.descr} />
-    )
+        )
+        : <p>Sprint Backlog is empty</p>;
     return (
         <div>
             <h1>SprintList</h1>
