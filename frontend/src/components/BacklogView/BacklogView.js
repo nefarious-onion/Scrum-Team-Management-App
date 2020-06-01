c40ff1348274961d7d3b949e92f28dc937e196cd
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-01 15:17:27 +0300

    add event handler callback

--- frontend/src/components/BacklogView/BacklogView.js
+++ frontend/src/components/BacklogView/BacklogView.js
@@ -2,11 +2,30 @@ import React, { useState, useEffect } from 'react';
 import BacklogList from './BacklogList/BacklogList';
 import SprintList from './SprintList/SprintList';
 import AddUserstoryForm from '../AddUserstoryForm/AddUserstoryForm';
-import { getStories, getStory } from '../../services/api.service';
+import { getStories, getStory, createStory } from '../../services/api.service';
+import './BacklogView.css';
 
 const BacklogView = () => {
     const [backlogList, setBacklogList] = useState([]);
     const [sprintlogList, setSprintlogList] = useState([]);
+
+    //helper function for fetching stories from database
+    const fetchStories = async () => {
+        const _stories = await getStories();
+        setBacklogList(_stories);
+        setSprintlogList(_stories);
+    }
+
+    //event callback function for creating new userstories
+    const onStoryCreate = async (storyInput) => {
+
+
+        //create new story and save it to db with API post request
+        //await createStory(newUserstory);
+        //fetch updated information from db and re-render the lists
+        //fetchStories();
+        console.log('story was sent to backlogview', storyInput);
+    }
 
     //fetching all of the stories from back-end
     useEffect(() => {
@@ -23,6 +42,7 @@ const BacklogView = () => {
 
     return (
         <div className="backlogview">
+            <AddUserstoryForm onStoryCreate={onStoryCreate}/>
             <BacklogList backloglist={backlogList} />
             <SprintList sprintloglist={sprintlogList} />
         </div>
