437f94f27938fad72c063b4f5a4ab2393839636f
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-02 17:16:34 +0300

    add event hadlers, icons, props

--- frontend/src/components/Userstory/Userstory.js
+++ frontend/src/components/Userstory/Userstory.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Userstory.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
-const Userstory = ({ title, _id, desc }) => {
-    //build userstory here
+const Userstory = ({ title, _id, desc, onStoryDelete, onStoryUpdate }) => {
+
+    const onDeleteClick = () => {
+       const storyId = _id;
+       console.log('clicked userstory', storyId);
+       onStoryDelete(storyId);
+    }
+    const onEditClick = () => {
+        const storyId = _id;
+        console.log('clicked userstory', storyId);
+        onStoryUpdate(storyId);
+    }
+    
     return (
         <div className='userstory-container'>
+             <FontAwesomeIcon icon={faEllipsisV} onClick={onEditClick} />
             <p className='userstory-title'>{title}</p>
-            {/* <p>{_id}</p>
-            <p>{desc}</p> */}
+            <FontAwesomeIcon icon={faTimes} onClick={onDeleteClick} />
         </div>
     );
 }
