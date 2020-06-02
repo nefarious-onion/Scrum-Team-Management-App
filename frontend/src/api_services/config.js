e185fadfc65c2b9789ce4328b269cf0c6b100d3d
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-02 12:59:40 +0300

    create config.js for shared config code

---
+++ frontend/src/api_services/config.js
@@ -0,0 +1,4 @@
+//simple dev environment check so after deployment we don't have manually change the url everytime
+const isDev = window.location.hostname === 'localhost';
+//if server is running on localhost, baseurl is set to be different from when it will be running on heroku
+export const baseUrl = isDev ? 'http://localhost:5000/api/' : '/api/';
