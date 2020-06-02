974fadb93489d67f8893540f32da9e8e06a58667
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-02 17:14:41 +0300

    add props to userstories

--- frontend/src/components/BacklogView/BacklogList/BacklogList.js
+++ frontend/src/components/BacklogView/BacklogList/BacklogList.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Userstory from '../../Userstory/Userstory';
 import './BacklogList.css';
 
-const BacklogList = ({ userstoryList, title }) => {
+const BacklogList = ({ userstoryList, title, onStoryDelete, onStoryUpdate }) => {
     //checks that the list is not empty or undefined
     const isValid = userstoryList !== undefined && userstoryList.length > 0;
 
     const storyList = isValid
         ? userstoryList.map(story =>
-            <Userstory key={story._id} title={story.title} _id={story._id} desc={story.descr} />)
+            <Userstory
+                key={story._id}
+                title={story.title}
+                _id={story._id}
+                desc={story.descr}
+                onStoryDelete={onStoryDelete}
+                onStoryUpdate={onStoryUpdate}
+            />)
         : <p>{title} is empty</p>;
 
     return (
