f2d29cd973cf4c500fd6850cb467d92a4b6f1d5c
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-03 13:57:23 +0300

    update axios calls for scrumlists

--- frontend/src/api_services/scrumlist.service.js
+++ frontend/src/api_services/scrumlist.service.js
@@ -3,17 +3,23 @@ import { baseUrl } from './config';
 
 const SCRUMLIST_URL = `${baseUrl}scrumlist`;
 
+//EXPORTED FUNCTIONS + PARAMETERS
+// getLists
+// getList -- listId
+
 //get all lists
+/**
+ * @returns {Promise} List of Scrumlists
+ */
 export const getLists = () => axios.get(SCRUMLIST_URL)
     .then(response => response.data)
     .catch(error => console.log('Something went wrong!', error));
 
 //get one list
+/**
+ * @param {string} listId list id from database
+ * @returns {Promise} single list
+ */
 export const getList = listId => axios.get(`${SCRUMLIST_URL}/${listId}`)
     .then(response => response.data)
     .catch(error => console.log('Something went wrong!', error));
-
-//update a list
-export const updateList = async () => {
-    
-}
