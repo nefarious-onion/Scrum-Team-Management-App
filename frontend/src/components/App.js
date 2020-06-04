9cc2550666a0fe0927b637732d6e64880a6a2b54
bea94k <beatakusnierz94@gmail.com>
2020-06-04 12:40:16 +0300

    Initial commit, fix <main> tags

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import BacklogView from './BacklogView/BacklogView';
-import ScrumboardView from './ScrumboardView/ScrumboardView'
-import Landing from './Landing/Landing'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import NavBar from './NavBar/NavBar'
+import ScrumboardView from './ScrumboardView/ScrumboardView';
+import Landing from './Landing/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar/NavBar';
 
 const App = () => {
-
-
-    return (
-        <Router>
-            <NavBar />
-            <div>
-                <Switch>
-                    <Route exact path='/' component={Landing} />
-                    <Route path='/backlog' component={BacklogView} />
-                    <Route path='/scrumboard' component={ScrumboardView} />
-                    <Route path='/meetings' />
-                    <Route path='/calendar' />
-                    <Route path='/overview' />
-                </Switch>
-            </div>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <NavBar />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/backlog" component={BacklogView} />
+          <Route path="/scrumboard" component={ScrumboardView} />
+          <Route path="/meetings" />
+          <Route path="/calendar" />
+          <Route path="/overview" />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
