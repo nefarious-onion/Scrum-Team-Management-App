d2c3dd7d552b2e65dc1d2c2a351372e000ba29ae
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-09 17:18:03 +0300

    fixed links to

--- frontend/src/components/AboutScrum/SideNavAbout/SideNavAbout.js
+++ frontend/src/components/AboutScrum/SideNavAbout/SideNavAbout.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Link, animateScroll as scroll } from "react-scroll";
 
 
-const SideNavAbout = ({ id }) => {
+const SideNavAbout = () => {
 
     return (
         <ul className="side-navbar">
             <li>
-                <Link className="side-navbar-item" to="section1" smooth={true}>What is scrum</Link>
+                <Link className="side-navbar-item" to="scrum" smooth={true}>What is scrum</Link>
             </li>
             <li>
-                <Link className="side-navbar-item" to="section1" smooth={true}>Roles</Link>
+                <Link className="side-navbar-item" to="roles" smooth={true}>Roles</Link>
             </li>
             <li>
-                <Link className="side-navbar-item" to="section1" smooth={true}>Workflow</Link>
+                <Link className="side-navbar-item" to="events" smooth={true}>Workflow</Link>
             </li>
             <li>
-                <Link className="side-navbar-item" to="section1" smooth={true}>Artifacts</Link>
+                <Link className="side-navbar-item" to="artifacts" smooth={true}>Artifacts</Link>
             </li>
 
         </ul >
