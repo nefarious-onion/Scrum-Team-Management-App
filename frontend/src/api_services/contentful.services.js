3d705a3e1d3ab19e6483d2e4bffd508ddafa3a78
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-09 11:11:30 +0300

    contentful.services and aboutscrum.css

--- frontend/src/api_services/contentful.services.js
+++ frontend/src/api_services/contentful.services.js
@@ -3,13 +3,12 @@ const client = require('contentful').createClient({
     space: 'n041sndlsw1t',
     accessToken: 'Bu4RwmRWubJ2JzRGJxE-uf1ikkfpCa7XoUgVMx12pnE'
 })
+//passes getAllEntries
 export const getAllEntries = () =>
     client.getEntries()
         .then(entries => entries.items)
-
-
         .catch(err => {
-            console.log(err + 'ACCESS DENIED! Check the file Contentful.js and getAllinfo')
+            console.log(err + 'ACCESS DENIED!')
         })
 
 
