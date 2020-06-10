b059ae0df050e119ec9e74678728c0b53c14ecce
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-10 22:02:55 +0300

    add new errorhandler

---
+++ backend/helpers/error.js
@@ -0,0 +1,29 @@
+class ErrorHandler extends Error {
+    constructor(statusCode, message) {
+        super()
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+
+const handleError = (err, res) => {
+    let { statusCode, message } = err;
+    if (!statusCode) {
+        statusCode = 500;
+    }
+
+    console.log({ statusCode, message })
+
+    res
+        .status(statusCode)
+        .json({
+            status: 'error',
+            statusCode,
+            message
+        })
+}
+
+module.exports = {
+    ErrorHandler,
+    handleError
+}
