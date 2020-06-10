7cb30b79437122b8d9f2c7b1f95b24a3c79e33d2
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-10 22:05:02 +0300

    add event.router, add middleware for error handling,

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
+//require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const isDev = process.env.NODE_ENV === 'development';
 const isProduction = process.env.NODE_ENV === 'production';
 const FRONTEND_ORIGIN = 'http://localhost:3000';
 
+const { handleError, Errorhandler } = require('./helpers/error');
 const dbService = require('./api/db/db.service');
 const userstoriesRouter = require('./api/userstories/userstories.router.js');
 const scrumlistsRouter = require('./api/scrumlists/scrumlists.router');
+const eventRouter = require('./api/calendar/event.router');
 
 const app = express();
 app.use(express.json());
@@ -37,6 +39,8 @@ if (isDev) {
 app.use('/api/scrumlist', scrumlistsRouter);
 // router for handling userstories
 app.use('/api/scrumlist/:list_id/userstory', userstoriesRouter);
+//router for events
+app.use('/api/event', eventRouter);
 
 //check if env is production => use static path
 if (isProduction) {
@@ -53,6 +57,15 @@ if (isProduction) {
 app.get('/ping', (req, res) => {
   res.send('hello world');
 });
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    handleError(err, res);
+  } else {
+    next()
+  }
+})
 
 app.listen(PORT, () => {
   console.log('Server started', PORT);
