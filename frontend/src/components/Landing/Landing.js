167108ce585cfb0e01b0f39f65b6cf7c055643c8
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-10 08:09:49 +0300

    fixed css

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -4,39 +4,39 @@ import './Landing.css';
 const Landing = () => {
   return (
     <>
-      <div className="landing-container">
-        <section >
-          <div className="hero-items">
-            <h1 className="hero-welcome"> lira</h1>
-            <p className="hero-content">Begin your path to mastering the Scrum method today!</p>
-          </div>
-          <div className="hero-img"> picture here</div>
-        </section>
-      </div>
-      <section className="">
-        <h1 className=""></h1>
-        <p className=""></p>
+      <section className='section-grid-2col'>
+        <div className='hero-left-container'>
+          <h1 className='hero-welcome'>Lira</h1>
+          <p className='hero-content'>thisislira</p>
+          <button className='hero-cta'>CTA</button>
+        </div>
+        <div className='hero-right-container'>
+          <p>hello im screenschot</p>
+        </div>
+
       </section>
-      <section className="">
-        <h1 className=""></h1>
-        <p className=""></p>
+      <section className='section-grid-2col'>
+        <div className='hero-left-container'>
+          <h1 className='hero-welcome'></h1>
+          <p className='hero-content'></p>
+        </div>
+        <div className='hero-right-container'>
+          <p>hello im screenschot</p>
+        </div>
       </section>
-      <section className="">
-        <h1 className="hero-welcome"></h1>
-        <p className="hero-content"></p>
+      <section className='section-grid-2col'>
+        <div className='hero-left-container'>
+          <h1 className='hero-welcome'></h1>
+          <p className='hero-content'></p>
+        </div>
+        <div className='hero-right-container'>
+          <p>hello im screenschot</p>
+        </div>
       </section>
-      <section className="hero-items">
-        <h1 className="hero-welcome"></h1>
-        <p className="hero-content"></p>
-      </section>
-      <section className="hero-items">
-        <h1 className="hero-welcome"></h1>
-        <p className="hero-content"></p>
-      </section>
-      <section className="hero-items">
-        <h1 className="hero-welcome"></h1>
-        <p className="hero-content"></p>
-      </section>
+      <section className='productInfo-container section-grid-2col'></section>
+      <section className='productInfo-container section-grid-2col'></section>
+      <section className='productInfo-container section-grid-2col'></section>
+      <section className='contact-container section-grid-2col'></section>
     </>
   );
 };
