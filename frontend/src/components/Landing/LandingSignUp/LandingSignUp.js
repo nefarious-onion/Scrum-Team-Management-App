332239735b630cae1188ed4a33a63f7ad403bc68
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-11 10:46:23 +0300

    check span thingy on last week polish

--- frontend/src/components/Landing/LandingSignUp/LandingSignUp.js
+++ frontend/src/components/Landing/LandingSignUp/LandingSignUp.js
@@ -9,6 +9,9 @@ const LandingSignUp = () => {
                 <label for="password"><span>password <span class="required">*</span></span><input type="text" class="input-field" name="password" value="" required /></label>
                 <label for="confirmPassword"><span>confirm password <span class="required">*</span></span><input type="text" class="input-field" name="confirmPassword" value="" required /></label>
                 <label><span> </span><input type="submit" value="Submit" /></label>
+                {
+                    //if span is removed, submit will go out of its slot - check this on last week polish
+                }
             </form>
         </div>
     );
