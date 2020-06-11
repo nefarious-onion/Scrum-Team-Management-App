46a9ea08628f53db51a0e6d92ed4d5c01bf5e097
bea94k <beatakusnierz94@gmail.com>
2020-06-11 12:08:46 +0300

    Inserted the code of Kanban Demo - Muuri into Backlog View

--- frontend/src/components/BacklogView/BacklogView.js
+++ frontend/src/components/BacklogView/BacklogView.js
@@ -2,138 +2,171 @@ import React, { useState, useEffect } from 'react';
 import BacklogList from './BacklogList/BacklogList';
 import AddUserstoryForm from '../AddUserstoryForm/AddUserstoryForm';
 import EditUserstoryForm from '../EditUserstoryForm/EditUserstoryForm';
-import { getStory, createStory, deleteStory, updateStory } from '../../api_services/userstory.service';
+import {
+  getStory,
+  createStory,
+  deleteStory,
+  updateStory,
+} from '../../api_services/userstory.service';
 import { backlogId, sprintlogId } from '../../api_services/config';
 import { getList, getLists } from '../../api_services/scrumlist.service';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import './BacklogView.css';
+import DragDropDemo from '../DragDropDemo/DragDrop';
 
 const BacklogView = () => {
-    const [backlogList, setBacklogList] = useState([]);
-    const [sprintlogList, setSprintlogList] = useState([]);
+  const [backlogList, setBacklogList] = useState([]);
+  const [sprintlogList, setSprintlogList] = useState([]);
 
-    const [currentList, setCurrentList] = useState('');
-    const [isAddFormVisible, setIsAddFormVisible] = useState(false);
-    const [isEditVisible, setIsEditVisible] = useState(false);
-    const [btnText, setBtnText] = useState('Add new userstory');
-    const [storyToEdit, setStoryToEdit] = useState('');
+  const [currentList, setCurrentList] = useState('');
+  const [isAddFormVisible, setIsAddFormVisible] = useState(false);
+  const [isEditVisible, setIsEditVisible] = useState(false);
+  const [btnText, setBtnText] = useState('Add new userstory');
+  const [storyToEdit, setStoryToEdit] = useState('');
 
-    //helper function for fetching stories from database and re-rendering the component
-    const fetchLists = async () => {
-        const _backlogList = await getList(backlogId);
-        const _sprintlogList = await getList(sprintlogId);
-        //sort the lists!!
-        setBacklogList(_backlogList.stories);
-        setSprintlogList(_sprintlogList.stories);
+  //helper function for fetching stories from database and re-rendering the component
+  const fetchLists = async () => {
+    const _backlogList = await getList(backlogId);
+    const _sprintlogList = await getList(sprintlogId);
+    //sort the lists!!
+    setBacklogList(_backlogList.stories);
+    setSprintlogList(_sprintlogList.stories);
+  };
+
+  //event callback function for creating new userstories
+  const onStoryCreate = async (listId, storyInput) => {
+    //create new story and save it to db with API post request
+    await createStory(listId, storyInput);
+    //fetch updated information from db and re-render the lists
+    fetchLists();
+  };
+  //deletes userstory and re-renders lists
+  const onStoryDelete = async (storyId) => {
+    await deleteStory(storyId);
+    fetchLists();
+  };
+  // get story to edit, open edit form
+  const getStoryForEdit = async (storyId, listName) => {
+    const _storyToEdit = await getStory(storyId);
+    if (_storyToEdit) {
+      setStoryToEdit(_storyToEdit);
+      setCurrentList(listName);
+      setIsEditVisible(true);
+    } else {
+      throw new Error('Something went wrong: Story was not found');
     }
+  };
+  //save edited userstory in edit form
+  const onStoryUpdate = async (storyId, updatedStory) => {
+    await updateStory(storyId, updatedStory);
 
-    //event callback function for creating new userstories
-    const onStoryCreate = async (listId, storyInput) => {
-        //create new story and save it to db with API post request
-        await createStory(listId, storyInput);
-        //fetch updated information from db and re-render the lists
-        fetchLists();
+    console.log('this story was edited: ', updatedStory);
+  };
+
+  const showUserstoryForm = () => {
+    if (!isAddFormVisible) {
+      setIsAddFormVisible(true);
+      setBtnText('Hide form');
     }
-    //deletes userstory and re-renders lists
-    const onStoryDelete = async (storyId) => {
-        await deleteStory(storyId);
-        fetchLists();
+    if (isAddFormVisible) {
+      setIsAddFormVisible(false);
+      setBtnText('Add new userstory');
     }
-    // get story to edit, open edit form
-    const getStoryForEdit = async (storyId, listName) => {
-        const _storyToEdit = await getStory(storyId);
-        if(_storyToEdit) {
-            setStoryToEdit(_storyToEdit);
-            setCurrentList(listName);
-            setIsEditVisible(true);
-        } else {
-            throw new Error('Something went wrong: Story was not found')
-        }
-    };
-    //save edited userstory in edit form
-    const onStoryUpdate = async (storyId, updatedStory) => {
-        await updateStory(storyId, updatedStory);
+  };
+  const onCloseEditForm = () => {
+    setIsEditVisible(false);
+    setStoryToEdit('');
+    fetchLists();
+  };
 
-        console.log('this story was edited: ', updatedStory);
-    }
+  //fetching all of the stories once when component is rendered
+  useEffect(() => {
+    getLists().then((lists) => console.log(lists));
 
-    const showUserstoryForm = () => {
-        if (!isAddFormVisible) {
-            setIsAddFormVisible(true);
-            setBtnText('Hide form');
-        }
-        if (isAddFormVisible) {
-            setIsAddFormVisible(false);
-            setBtnText('Add new userstory');
-        }
-    }
-    const onCloseEditForm = () => {
-        setIsEditVisible(false);
-        setStoryToEdit('');
-        fetchLists();
-    }
+    //sort the lists!!
+    getList(backlogId)
+      .then((list) => {
+        console.log(list.title);
+        setBacklogList(list.stories);
+      })
+      .catch((error) => {
+        console.log(new Error(error));
+      });
+    getList(sprintlogId)
+      .then((list) => {
+        console.log(list.title);
+        setSprintlogList(list.stories);
+      })
+      .catch((error) => {
+        console.log(new Error(error));
+      });
+  }, []);
 
-    //fetching all of the stories once when component is rendered
-    useEffect(() => {
-        getLists()
-            .then(lists => console.log(lists));
-
-        //sort the lists!!
-        getList(backlogId)
-            .then(list => {
-                console.log(list.title)
-                setBacklogList(list.stories)
-            })
-            .catch((error) => {
-                console.log(new Error(error))
-            })
-        getList(sprintlogId)
-            .then(list => {
-                console.log(list.title)
-                setSprintlogList(list.stories)
-            })
-            .catch((error) => {
-                console.log(new Error(error))
-            })
-    }, []);
-
-    return (
-        <>
-            {isEditVisible ? < EditUserstoryForm listName={currentList} onStoryDelete={onStoryDelete} onStoryUpdate={onStoryUpdate} storyToEdit={storyToEdit} onCloseEditForm={onCloseEditForm} /> : null}
-            <div className="backlogview-container">
-                <div className='backloglist-wrapper productBacklog-light'>
-                    <div className='backloglist__header-wrapper'>
-                        <FontAwesomeIcon icon={faEllipsisH} />
-                        <h1 className='backloglist__header'>Product Backlog</h1>
-                        <button className='add-userstory-btn' onClick={showUserstoryForm} >{btnText}</button>
-                    </div>
-                    {isAddFormVisible ? <AddUserstoryForm onStoryCreate={onStoryCreate} listId={backlogId} /> : null}
-                    <BacklogList 
-                        userstoryList={backlogList} 
-                        title='Product Backlog' 
-                        onStoryDelete={onStoryDelete} 
-                        getStoryForEdit={storyId => getStoryForEdit(storyId, 'product backlog')} 
-                    />
-                </div>
-                <div className='backloglist-wrapper sprintBacklog-light'>
-                <div className='backloglist__header-wrapper'>
-                        <FontAwesomeIcon icon={faEllipsisH} />
-                        <h1 className='backloglist__header'>Sprint Backlog</h1>
-                        <button className='add-userstory-btn' onClick={showUserstoryForm} >{btnText}</button>
-                    </div>
-                    {isAddFormVisible ? <AddUserstoryForm onStoryCreate={onStoryCreate} listId={sprintlogId} /> : null}
-                    {/* <h1 className='backloglist__header' >Sprint Backlog</h1> */}
-                    <BacklogList 
-                        userstoryList={sprintlogList} 
-                        title='Sprint Backlog' 
-                        onStoryDelete={onStoryDelete} 
-                        g getStoryForEdit={storyId => getStoryForEdit(storyId, 'sprint backlog')} 
-                    />
-                </div>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      {isEditVisible ? (
+        <EditUserstoryForm
+          listName={currentList}
+          onStoryDelete={onStoryDelete}
+          onStoryUpdate={onStoryUpdate}
+          storyToEdit={storyToEdit}
+          onCloseEditForm={onCloseEditForm}
+        />
+      ) : null}
+      <div className="backlogview-container">
+        <div className="backloglist-wrapper productBacklog-light">
+          <div className="backloglist__header-wrapper">
+            <FontAwesomeIcon icon={faEllipsisH} />
+            <h1 className="backloglist__header">Product Backlog</h1>
+            <button className="add-userstory-btn" onClick={showUserstoryForm}>
+              {btnText}
+            </button>
+          </div>
+          {isAddFormVisible ? (
+            <AddUserstoryForm
+              onStoryCreate={onStoryCreate}
+              listId={backlogId}
+            />
+          ) : null}
+          <BacklogList
+            userstoryList={backlogList}
+            title="Product Backlog"
+            onStoryDelete={onStoryDelete}
+            getStoryForEdit={(storyId) =>
+              getStoryForEdit(storyId, 'product backlog')
+            }
+          />
+        </div>
+        <div className="backloglist-wrapper sprintBacklog-light">
+          <div className="backloglist__header-wrapper">
+            <FontAwesomeIcon icon={faEllipsisH} />
+            <h1 className="backloglist__header">Sprint Backlog</h1>
+            <button className="add-userstory-btn" onClick={showUserstoryForm}>
+              {btnText}
+            </button>
+          </div>
+          {isAddFormVisible ? (
+            <AddUserstoryForm
+              onStoryCreate={onStoryCreate}
+              listId={sprintlogId}
+            />
+          ) : null}
+          {/* <h1 className='backloglist__header' >Sprint Backlog</h1> */}
+          <BacklogList
+            userstoryList={sprintlogList}
+            title="Sprint Backlog"
+            onStoryDelete={onStoryDelete}
+            g
+            getStoryForEdit={(storyId) =>
+              getStoryForEdit(storyId, 'sprint backlog')
+            }
+          />
+        </div>
+      </div>
+      <DragDropDemo />
+    </>
+  );
+};
 
 export default BacklogView;
---
+++ frontend/src/components/DragDropDemo/DragDrop.js
@@ -0,0 +1,72 @@
+/* React */
+import React, { useState } from 'react';
+/* Muuri-react */
+import { MuuriComponent, useGrid } from 'muuri-react';
+/* Utils & components */
+import { boardOptions, columnOptions, getRandomWord, useSend } from './utils';
+import { Column, Header, Demo } from './demoComp';
+/* Style */
+import './style.css';
+
+// DragDropDemo
+const DragDrop = () => {
+  // Items state.
+  const [items, setItems] = useState({
+    todo: ['4', '6', '5', '10'],
+    done: ['2', '7', '9'],
+  });
+
+  // UseSend is used when a item changes grid
+  // to sync the items state.
+  const onSend = useSend(setItems);
+
+  // Children.
+  const children = {
+    todo: items.todo.map((id) => <Item id={id} key={id} />),
+    done: items.done.map((id) => <Item id={id} key={id} />),
+  };
+
+  return (
+    <Demo>
+      {/*  <Header /> */}
+      {/* Columns container */}
+      <MuuriComponent {...boardOptions}>
+        {/* 'To do' column */}
+        <Column actionClass="todo" title="To do">
+          {/* Column content */}
+          <MuuriComponent id={'TODO'} onSend={onSend} {...columnOptions}>
+            {children.todo}
+          </MuuriComponent>
+        </Column>
+        {/* 'Done' column */}
+        <Column actionClass="done" title="Done">
+          {/* Column content */}
+          <MuuriComponent id={'DONE'} onSend={onSend} {...columnOptions}>
+            {children.done}
+          </MuuriComponent>
+        </Column>
+      </MuuriComponent>
+    </Demo>
+  );
+};
+
+// Item component.
+// Some memoization to make the app lighter.
+const Item = React.memo(({ id }) => {
+  // State is manteined when an item change parent.
+  const [tag] = useState(getRandomWord());
+  // Get the MuuriComponent parent id.
+  const gridId = useGrid().id.toLowerCase();
+
+  return (
+    <div className="board-item">
+      <div className="board-item-content">
+        <span>Item </span>
+        {`${id} - ${tag}`}
+        <div className={`tab-item ${gridId}-tab-item`} />
+      </div>
+    </div>
+  );
+});
+
+export default DragDrop;
