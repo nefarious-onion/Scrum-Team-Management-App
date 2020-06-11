a88ea4d14e2a298dc5ecce84b7748f2e6f3b06ed
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-11 09:05:19 +0300

    css fixes, adding content

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Landing.css';
+import LandingSignUp from '../Landing/LandingSignUp/LandingSignUp'
 
 const Landing = () => {
   return (
@@ -7,66 +8,55 @@ const Landing = () => {
       <section className='section-grid-2col'>
         <div className='hero-left-container'>
           <h2 className='hero-welcome'>Lira</h2>
-          <p className='hero-content'>Begin your path to mastering the Scrum method today!</p>
-          <button className='hero-cta'>CTA</button>
+          <p className='hero-cta-content'>Begin your path to mastering the Scrum method today!</p>
+          <button className='hero-cta-btn'>get started!</button>
         </div>
         <div className='hero-right-container'>
-          <p>hello im screenschot</p>
-        </div>
-
-      </section>
-      <section className='section-grid-2col'>
-        <div className='hero-left-container'>
-          <p className='hero-content'></p>
-        </div>
-        <div className='hero-right-container'>
-          <p>hello im screenschot</p>
+          <p>こんにちは私の名前はコンテンツです</p>
         </div>
       </section>
       <section className='section-grid-2col'>
-        <div className='hero-left-container'>
-        
-          <p className='hero-content'></p>
+        <div className='marketing-content-left-col'>
+          <p>こんにちは私の名前はコンテンツです</p>
         </div>
-        <div className='hero-right-container'>
-          <p>hello im screenschot</p>
+        <div className='marketing-content-right-col'>
+          <p>Get to know the basics of the Scrum method - just as much as you need to start and learn more as you go!</p>
+        </div>
+      </section>
+      <section className='section-grid-2col'>
+        <div className='marketing-content-left-col'>
+          <p>Forgot what you've just read in the info page ? Our quick tips got you covered</p>
+        </div>
+        <div className='marketing-content-right-col'>
+          <p>こんにちは私の名前はコンテンツです</p>
+        </div>
+      </section>
+      <section className='section-grid-2col'>
+        <div className='marketing-content-left-col'>
+          <p>こんにちは私の名前はコンテンツです</p>
+        </div>
+        <div className='marketing-content-right-col'>
+          <p>Easily write userstories and move them around between the scrum lists</p>
+        </div>
+      </section>
+      <section className='section-grid-2col'>
+        <div className='marketing-content-left-col'>
+          <p>Faciliate meetings like a boss... even thought there are no bosses in the Scrum method</p>
+        </div>
+        <div className='marketing-content-right-col'>
+          <p>こんにちは私の名前はコンテンツです</p>
         </div>
       </section>
       <section className='productInfo-container section-grid-2col'>
-      <div className='hero-left-container'>
-          
-          <p className='hero-content'></p>
+        <div className='marketing-content-left-col'>
+          <p>こんにちは私の名前はコンテンツです</p>
         </div>
-        <div className='hero-right-container'>
-          <p>hello im screenschot</p>
-        </div>
-      </section>
-      <section className='productInfo-container section-grid-2col'>
-      <div className='hero-left-container'>
-          
-          <p className='hero-content'></p>
-        </div>
-        <div className='hero-right-container'>
-          <p>hello im screenschot</p>
+        <div className='marketing-content-right-col'>
+          <p>Put your scheduled meetings in the in-app calendar for all your teammates to see</p>
         </div>
       </section>
-      <section className='productInfo-container section-grid-2col'>
-      <div className='hero-left-container'>
-          
-          <p className='hero-content'></p>
-        </div>
-        <div className='hero-right-container'>
-          <p>hello im screenschot</p>
-        </div>
-      </section>
-      <section className='contact-container section-grid-2col'>
-      <div className='hero-left-container'>
-          <h1 className='hero-welcome'></h1>
-          <p className='hero-content'></p>
-        </div>
-        <div className='hero-right-container'>
-          <p>hello im contact</p>
-        </div>
+      <section className='contact'>
+        <LandingSignUp />
       </section>
     </>
   );
--- frontend/src/components/Landing/LandingSignUp/LandingSignUp.js
+++ frontend/src/components/Landing/LandingSignUp/LandingSignUp.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './LandingSignUp.css'
 const LandingSignUp = () => {
     return (
-        <div>
-
+        <div class="contact-form">
+            <div class="contact-form-heading">Sign up today!</div>
+            <form action="" method="post">
+                <label for="field1"><span>email<span class="required" >*</span></span><input type="text" class="input-field" name="field1" value="" required /></label>
+                <label for="field2"><span>passowrd <span class="required">*</span></span><input type="text" class="input-field" name="field2" value="" required /></label>
+                <label for="field3"><span>confirm passowrd <span class="required">*</span></span><input type="text" class="input-field" name="field3" value="" required /></label>
+                <label><span> </span><input type="submit" value="Submit" /></label>
+            </form>
         </div>
     );
 }
