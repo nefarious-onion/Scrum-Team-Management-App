3dd10b3520bb3299504155366256dc004706a505
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-12 10:54:52 +0300

    import EventCalendar component, add array of events and timezone

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EventCalendar from './EventCalendar';
+import './Calendar.css';
+
+let events = [
+    {title: "This is an event", start: "2020-06-17T10:30:00", end:  "2020-06-18T17:30:00"},
+    { title: 'event 1', date: '2020-07-01', daysOfWeek: [1, 2, 3] },
+    { title: 'event 2', date: '2020-06-24', color: "hotpink", description: "this is a new event"}
+]
 
 const Calendar = () => {
+    const [timeZone, setTimezone] = useState('EEST');
+    
     return (
         <div>
-            This is calendar
+           <EventCalendar events={events} timeZone={timeZone}/>
         </div>
     );
 }
