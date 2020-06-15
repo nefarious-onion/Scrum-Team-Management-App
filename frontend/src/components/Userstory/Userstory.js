b92639b4eb52defbb0f215df17a3bf5ff247c947
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-16 01:25:57 +0300

    add draggable

--- frontend/src/components/Userstory/Userstory.js
+++ frontend/src/components/Userstory/Userstory.js
@@ -2,28 +2,40 @@ import React from 'react';
 import './Userstory.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { Draggable } from 'react-beautiful-dnd';
 
-const Userstory = ({ title, id, desc, onStoryDelete, getStoryForEdit }) => {
+const Userstory = ({ title, id, desc, onStoryDelete, getStoryForEdit, index }) => {
 
     const onClickDelete = () => {
-       const storyId = id;
-       console.log('clicked userstory', storyId);
-       //defined in backlogview
-       onStoryDelete(storyId);
+        const storyId = id;
+        console.log('clicked userstory', storyId);
+        //defined in backlogview
+        onStoryDelete(storyId);
     }
     const onClickEdit = () => {
         const storyId = id;
         console.log('clicked userstory', storyId);
-         //sends userstoryId to backlogview
+        //sends userstoryId to backlogview
         getStoryForEdit(storyId);
     }
-    
+
     return (
-        <div className='userstory-container'>
-             <FontAwesomeIcon icon={faEllipsisV} onClick={onClickEdit} />
-            <p className='userstory-title'>{title}</p>
-            <FontAwesomeIcon icon={faTimes} onClick={onClickDelete} />
-        </div>
+        <Draggable draggableId={id} index={index} >
+            {(provided) => (
+                <div className='userstory-container'
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                >
+                    <FontAwesomeIcon icon={faEllipsisV} onClick={onClickEdit} />
+                    <p className='userstory-title'>{title}</p>
+                    <FontAwesomeIcon icon={faTimes} onClick={onClickDelete} />
+                </div>
+            )
+
+            }
+
+        </Draggable>
     );
 }
 
