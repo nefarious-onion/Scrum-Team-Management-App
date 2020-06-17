3c9a536ec197a2460f10b5721dda5bbde209cf9b
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-17 15:46:07 +0300

    style edit form

--- frontend/src/components/Calendar/EditEventForm.js
+++ frontend/src/components/Calendar/EditEventForm.js
@@ -15,23 +15,23 @@ const weekdays = {
 }
 
 const EditEventForm = ({ eventToEdit, onCloseEventForm, onEventDelete }) => {
-    const { title, start, end, allDay, dateStr, extendedProps } = eventToEdit;
-    console.log(eventToEdit);
-    const id = extendedProps._id;
-    console.log(id)
-    const day = start.getUTCDay();
-    const weekday = weekdays[day];
-    const formattedStart = start.toLocaleDateString();
-    const formattedEnd = null;
-    if (end) {
-         formattedEnd = end.toLocaleDateString();
-    }
+    //const { title, start, end, allDay, dateStr, extendedProps } = eventToEdit;
+    // console.log(eventToEdit);
+    // const id = extendedProps._id;
+    // console.log(id)
+    // const day = start.getUTCDay();
+    // const weekday = weekdays[day];
+    // const formattedStart = start.toLocaleDateString();
+    // const formattedEnd = null;
+    // if (end) {
+    //      formattedEnd = end.toLocaleDateString();
+    // }
     
 
     const onDeleteClick = () => {
-        if (window.confirm('Are you sure you want to delete the event?')) {
-            onEventDelete(id);
-        }
+        // if (window.confirm('Are you sure you want to delete the event?')) {
+        //     onEventDelete(id);
+        // }
     }
 
 
@@ -40,16 +40,16 @@ const EditEventForm = ({ eventToEdit, onCloseEventForm, onEventDelete }) => {
             <div className='event-form__overlay' onClick={onCloseEventForm}></div>
             <div className='event-form__container'>
                 <div className='event-form__editpanel'>
-                    <h2>Edit event</h2>
-                    <FontAwesomeIcon icon={faTrashAlt} size='2x' onClick={onDeleteClick} />
-                    <FontAwesomeIcon icon={faTimes} size='2x' onClick={onCloseEventForm} />
+                    <p className='event-header'>Edit event</p>
+                    <FontAwesomeIcon icon={faTrashAlt} size='1x' onClick={onDeleteClick} />
+                    <FontAwesomeIcon icon={faTimes} size='1x' onClick={onCloseEventForm} />
                 </div>
                 <div className='event-form__content'>
-                    <h3>{title}</h3>
-                    <h4>{dateStr}</h4>
-                    <h4>{weekday}</h4>
+                    {/* <p>{title}</p>
+                    <p>{dateStr}</p>
+                    <p>{weekday}</p>
                     {allDay ? <h4>All day event</h4> : <h4>{formattedStart}</h4>}
-                    {end ? <h4>{formattedEnd}</h4> : null}
+                    {end ? <h4>{formattedEnd}</h4> : null} */}
                 </div>
 
             </div>
