3dabd8dce25a7d1b99dace46b61686ed3524aa31
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-18 06:57:58 +0300

    footer imported, links, little css

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './Footer.css'
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <ul>
+                    <li>
+                        <a href="https://github.com/nefarious-onion" target="_blank">Anne's Github</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/bea94k" target="_blank">Bea's Github</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/jpaljakka" target="_blank">Jonne's Github</a>
+                    </li>
+                </ul>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
