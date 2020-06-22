3d0a3c58b016ae5b51225d83ae360188f4ec82fe
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-06-22 13:46:40 +0300

    footer import

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,17 +5,7 @@ const Footer = () => {
     return (
         <>
             <footer>
-                <ul className='footer_container'>
-                    <li className='footer_item first_link'>
-                        <a className='footer_item' href="https://github.com/nefarious-onion" target="_blank">Anne's Github</a>
-                    </li>
-                    <li className='footer_item second_link'>
-                        <a className='footer_item' href="https://github.com/bea94k" target="_blank">Bea's Github</a>
-                    </li >
-                    <li className='footer_item third_link'>
-                        <a className='footer_item' href="https://github.com/jpaljakka" target="_blank">Jonne's Github</a>
-                    </li>
-                </ul>
+                <h6>© 2020 Anne Kokkonen, Jonne Paljakka and Bea Kuśnierz</h6>
             </footer>
         </>
     );
